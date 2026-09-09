10215f2590cb7583ae5fe9a17144233895d4eab3
ali123nezhadali <alinezhadali021@gmail.com>
2026-09-09 17:12:00 +0330

    Add files via upload

---
+++ Forosh/FO_rpt_ForoshandehAdamPishFaktor/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FO_rpt_ForoshandehAdamPishFaktor")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("UD")> 
+<Assembly: AssemblyProduct("FO_rpt_ForoshandehAdamPishFaktor")> 
+<Assembly: AssemblyCopyright("Copyright © UD 2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f880a7d9-b523-43b1-97db-4d6090ede4a9")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Forosh/FO_rpt_ForoshandehNobatVizit/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FO_rpt_ForoshandehNobatVizit")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("UD")> 
+<Assembly: AssemblyProduct("FO_rpt_ForoshandehNobatVizit")> 
+<Assembly: AssemblyCopyright("Copyright © UD 2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("352e5f24-cb50-4169-82c3-85f9ea59137c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
