6e3bc739d9c942ae697f1f4080949791a71b9f04
ali123nezhadali <alinezhadali021@gmail.com>
2026-09-12 09:38:47 +0330

    1

---
+++ Forosh/FO_rpt_Faktor/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FO_rpt_Faktor")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("UD")> 
+<Assembly: AssemblyProduct("FO_rpt_Faktor")> 
+<Assembly: AssemblyCopyright("Copyright © UD 2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("80916f1d-048f-4219-b5b1-4b1192cb5a5d")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
